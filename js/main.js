fce5093c460e269d89e328e0f32e50bce2f11a37
Taz17 <86769131+Taz17@users.noreply.github.com>
2021-10-20 12:44:44 +1100

    Updated data, webpage, and styling

--- js/main.js
+++ js/main.js
@@ -1,21 +1,42 @@
-var viz = 'js/visualizations/Commute.vg.json';
+var viz = 'js/visualizations/australia-visualizations/Commute.vg.json';
 vegaEmbed('#embed_bar_chart', viz)
     .then(function (result) {})
     .catch(console.error);
 
-load_button = document.getElementById('load-viz');
-select = document.getElementById('bar_chart_selector');
+var viz =
+    'js/visualizations/country-map-visualizations/Freedom-to-make-life-choices-visualization.vg.json';
+vegaEmbed('#embed_country_choropleth', viz)
+    .then(function (result) {})
+    .catch(console.error);
 
-load_button.addEventListener('click', () => {
+load_bar_button = document.getElementById('load-bar-viz');
+bar_chart_select = document.getElementById('bar_chart_selector');
+
+load_bar_button.addEventListener('click', () => {
     // console.log('clicked');
 
-    const filename = select.options[select.selectedIndex].value;
+    const filename =
+        bar_chart_select.options[bar_chart_select.selectedIndex].value;
     // console.log('filename: ' + filename);
 
-    const viz_selected = `js/visualizations/${filename}.vg.json`;
+    const viz_selected = `js/visualizations/australia-visualizations/${filename}.vg.json`;
     // console.log(viz_selected);
 
     vegaEmbed('#embed_bar_chart', viz_selected)
+        .then(function (result) {})
+        .catch(console.error);
+});
+
+load_country_choro = document.getElementById('load-choro-countries');
+choropleth_select = document.getElementById('choropleth_selector');
+
+load_country_choro.addEventListener('click', () => {
+    const filename =
+        choropleth_select.options[choropleth_select.selectedIndex].value;
+
+    const viz_selected = `js/visualizations/country-map-visualizations/${filename}.vg.json`;
+
+    vegaEmbed('#embed_country_choropleth', viz_selected)
         .then(function (result) {})
         .catch(console.error);
 });
