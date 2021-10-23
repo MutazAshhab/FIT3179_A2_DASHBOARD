d3f3a14fabd8ecab240e3e630c1b898f0f49fdce
Taz17 <86769131+Taz17@users.noreply.github.com>
2021-10-23 14:32:35 +1100

    added more js and updated page

--- js/main.js
+++ js/main.js
@@ -19,18 +19,16 @@ load_bar_button = document.getElementById('load-bar-viz');
 bar_chart_select = document.getElementById('bar_chart_selector');
 
 load_bar_button.addEventListener('click', () => {
-    // console.log('clicked');
-
     const filename =
         bar_chart_select.options[bar_chart_select.selectedIndex].value;
-    // console.log('filename: ' + filename);
 
-    const viz_selected = `js/visualizations/australia-visualizations/${filename}.vg.json`;
-    // console.log(viz_selected);
+    if (filename != '0') {
+        const viz_selected = `js/visualizations/australia-visualizations/${filename}.vg.json`;
 
-    vegaEmbed('#embed_bar_chart', viz_selected)
-        .then(function (result) {})
-        .catch(console.error);
+        vegaEmbed('#embed_bar_chart', viz_selected)
+            .then(function (result) {})
+            .catch(console.error);
+    }
 });
 
 load_country_choro = document.getElementById('load-choro-countries');
@@ -39,26 +37,28 @@ choropleth_select = document.getElementById('choropleth_selector');
 load_country_choro.addEventListener('click', () => {
     const filename =
         choropleth_select.options[choropleth_select.selectedIndex].value;
+    if (filename != '0') {
+        const viz_selected = `js/visualizations/country-map-visualizations/${filename}.vg.json`;
 
-    const viz_selected = `js/visualizations/country-map-visualizations/${filename}.vg.json`;
-
-    vegaEmbed('#embed_country_choropleth', viz_selected)
-        .then(function (result) {})
-        .catch(console.error);
+        vegaEmbed('#embed_country_choropleth', viz_selected)
+            .then(function (result) {})
+            .catch(console.error);
+    }
 });
 
-load_continent_choro = document.getElementById('load-choro-countries');
-choropleth_select_continent = document.getElementById('choropleth_selector');
+load_continent_choro = document.getElementById('load-choro-continents');
+choropleth_select_continent = document.getElementById('continent_selector');
 
 load_continent_choro.addEventListener('click', () => {
     const filename =
         choropleth_select_continent.options[
             choropleth_select_continent.selectedIndex
         ].value;
+    if (filename != '0') {
+        const viz_selected = `js/visualizations/continent-map-visualizations/${filename}.vg.json`;
 
-    const viz_selected = `js/visualizations/continent-map-visualizations/${filename}.vg.json`;
-
-    vegaEmbed('#embed_continent_choropleth', viz_selected)
-        .then(function (result) {})
-        .catch(console.error);
+        vegaEmbed('#embed_continent_choropleth', viz_selected)
+            .then(function (result) {})
+            .catch(console.error);
+    }
 });
